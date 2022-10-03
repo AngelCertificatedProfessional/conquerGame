d8c1b46218f68031111517bc3447311e900891a3
Liuts213 <angel.espinozatorres213@gmail.com>
2022-10-03 16:54:10 -0700

    agregacion del primer codigo

---
+++ app.js
@@ -0,0 +1,6 @@
+
+import { configuracionJuego } from './pieces/configuracionJuego.js'
+
+addEventListener( 'DOMContentLoaded', _ => {
+    configuracionJuego.renderPieces()
+})
---
+++ pieces/configuracionJuego.js
@@ -0,0 +1,42 @@
+import { initialGame } from '../config/initialGame.js'
+import { piecesImages } from '../config/piecesImages.js'
+export const configuracionJuego = {
+    
+    //Metodo que inicializa el juego
+    renderPieces() {
+        const gameSetup = true ? initialGame : potentialGame
+
+        this.placePieceBoxNumbers()
+        this.placeWhiteDownOrUp()
+        this.placePiecesInPosition( gameSetup )
+        // this.addPiecesBoxListeners()
+        //this.piecesDetermine()
+    },
+    placePieceBoxNumbers() {
+        
+        //console.log([ ...document.querySelectorAll( '.piece-box' ) ]);
+        ([ ...document.querySelectorAll( '.piece-box' ) ] ).map( pieceBoxElement => {
+            const spanElement = document.createElement( 'span' )
+            spanElement.classList.add( 'piece-box-text' )
+            spanElement.innerHTML = pieceBoxElement.getAttribute( 'id' )    
+            pieceBoxElement.append( spanElement )
+        })
+    },
+    placeWhiteDownOrUp() {
+        const flexWrap = false ? 'wrap' : 'wrap-reverse'
+        document.querySelector( '.chess-table' ).style.flexWrap = flexWrap;
+    },
+    placePiecesInPosition( gameSetup ) {
+        for ( const piecePosition in gameSetup ) {
+            const pieceType = gameSetup[ piecePosition ]
+            const pieceImageLocation = piecesImages[ pieceType ]
+
+            const imgElement = document.createElement( 'img' )
+            imgElement.classList.add( 'piece' )
+            imgElement.setAttribute( 'piece-type', pieceType )
+            imgElement.src = `${ pieceImageLocation }`
+
+            $( `#${ piecePosition }` ).append( imgElement )
+        }
+    },
+}
