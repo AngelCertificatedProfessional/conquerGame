7c12e4582931e3a973963c99847de9c48aeb9656
Liuts213 <angel.espinozatorres213@gmail.com>
2022-10-12 16:32:28 -0700

    se agrego el segmento de configuracion juego y services

--- pieces/configuracionJuego.js
+++ pieces/configuracionJuego.js
@@ -1,15 +1,16 @@
-import { initialGame } from '../config/initialGame.js'
-import { piecesImages } from '../config/piecesImages.js'
+import { initialGame } from '../config/initialGame.js';
+import { piecesImages } from '../config/piecesImages.js';
+import { piecesHandle } from '../services/piecesHandle.js';
 export const configuracionJuego = {
-    
+    piecesEventListeners: {},
     //Metodo que inicializa el juego
     renderPieces() {
         const gameSetup = true ? initialGame : potentialGame
 
         this.placePieceBoxNumbers()
-        this.placeWhiteDownOrUp()
+        //this.placeWhiteDownOrUp()
         this.placePiecesInPosition( gameSetup )
-        // this.addPiecesBoxListeners()
+        this.addPiecesBoxListeners()
         //this.piecesDetermine()
     },
     placePieceBoxNumbers() {
@@ -22,10 +23,10 @@ export const configuracionJuego = {
             pieceBoxElement.append( spanElement )
         })
     },
-    placeWhiteDownOrUp() {
-        const flexWrap = false ? 'wrap' : 'wrap-reverse'
-        document.querySelector( '.chess-table' ).style.flexWrap = flexWrap;
-    },
+    // placeWhiteDownOrUp() {
+    //     const flexWrap = false ? 'wrap' : 'wrap-reverse'
+    //     document.querySelector( '.chess-table' ).style.flexWrap = flexWrap;
+    // },
     placePiecesInPosition( gameSetup ) {
         for ( const piecePosition in gameSetup ) {
             const pieceType = gameSetup[ piecePosition ]
@@ -38,5 +39,35 @@ export const configuracionJuego = {
 
             document.querySelector(`#${ piecePosition }` ).append( imgElement )
         }
+    },
+    addPiecesBoxListeners() {
+        document.querySelectorAll('.piece-box').forEach( pieceBoxElement => {
+            const pieceBoxPosition = pieceBoxElement.getAttribute( 'id' )
+            const pieceElement = pieceBoxElement.querySelector('.piece' )
+            const pieceType = pieceElement?.getAttribute( 'piece-type' ) ?? null
+
+            const handleParams = {
+                pieceBoxElement,
+                pieceBoxPosition,
+                pieceElement,
+                pieceType
+            }
+
+            this.piecesEventListeners[ pieceBoxPosition ] = {
+                'mouseenter': _ => {
+                    piecesHandle.handlePieceMouseenter( handleParams )
+                },
+                'mouseleave': _ => {
+                    piecesHandle.handlePieceMouseleave( handleParams )
+                },
+                'click': _ => {
+                    piecesHandle.handlePieceClick( handleParams )
+                },
+            }
+
+        //     pieceBoxElement.addEventListener( 'mouseenter', this.piecesEventListeners[ pieceBoxPosition ][ 'mouseenter' ])
+        //     pieceBoxElement.addEventListener( 'mouseleave', this.piecesEventListeners[ pieceBoxPosition ][ 'mouseleave' ])
+        //     pieceBoxElement.addEventListener( 'click', this.piecesEventListeners[ pieceBoxPosition ][ 'click' ])
+        })
     },
 }
