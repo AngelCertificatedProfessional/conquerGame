044dce5f5d9f7e79825d9867ac699c55a8dac29d
Liuts213 <angel.espinozatorres213@gmail.com>
2022-10-28 07:46:25 -0700

    se modifico las habilidades del rey para poder moverse entre dos espacios a la vez

--- js/piezas/rey.js
+++ js/piezas/rey.js
@@ -1,50 +1,103 @@
 import {alfabetoANumero, colorOpciones,numeroAAlfabeto,tamanoTableroAncho,tamanoTableroLargo,validaPiezaMontana} from '../util/configuracionGeneral.js'
 export const movimientoRey= (row,col,item) =>{
     let nCol = alfabetoANumero(col) 
-    if (nCol < tamanoTableroAncho) {
-        if(!validaPiezaMontana(`${row}${numeroAAlfabeto(nCol+1)}`)){
-            console.log(row+numeroAAlfabeto(nCol+1))
-            document.getElementById(`${row}${numeroAAlfabeto(nCol+1)}`).style.backgroundColor = colorOpciones
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row+i}${numeroAAlfabeto(nCol+i)}`)){
+            break
+        }
+        if ((row+i) < tamanoTableroLargo+1 && (nCol+i) < tamanoTableroAncho+1 && document.getElementById(`${row+i}${numeroAAlfabeto(nCol+i)}`).innerText == 0) {
+            document.getElementById(`${row+i}${numeroAAlfabeto(nCol+i)}`).style.backgroundColor = colorOpciones
+        }else if ((row+i) < tamanoTableroLargo+1 && (nCol+i) < tamanoTableroAncho+1 && document.getElementById(`${row+i}${numeroAAlfabeto(nCol+i)}`).innerText !== 0) {
+            document.getElementById(`${row+i}${numeroAAlfabeto(nCol+i)}`).style.backgroundColor = colorOpciones
+            break
         }
     }
-    //Oeste
-    if (nCol > 1) {
-        if(!validaPiezaMontana(`${row}${numeroAAlfabeto(nCol-1)}`)){
-            document.getElementById(`${row}${numeroAAlfabeto(nCol-1)}`).style.backgroundColor = colorOpciones
+    //sureste
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row-i}${numeroAAlfabeto(nCol+i)}`)){
+            break
+        }
+        if ((row-i) >= 1 && (nCol+i) < tamanoTableroAncho+1 && document.getElementById(`${row-i}${numeroAAlfabeto(nCol+i)}`).innerText == 0) {
+            document.getElementById(`${row-i}${numeroAAlfabeto(nCol+i)}`).style.backgroundColor = colorOpciones
+        }else if ((row-i) >= 1 && (nCol+i) < tamanoTableroAncho+1&& document.getElementById(`${row-i}${numeroAAlfabeto(nCol+i)}`).innerText !== 0) {
+            document.getElementById(`${row-i}${numeroAAlfabeto(nCol+i)}`).style.backgroundColor = colorOpciones
+            break
         }
     }
+    //noroeste
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row+i}${numeroAAlfabeto(nCol-i)}`)){
+            break
+        }
+        if ((row+i) < tamanoTableroLargo+1 && (nCol-i) >= 1 && document.getElementById(`${row+i}${numeroAAlfabeto(nCol-i)}`).innerText == 0) {
+            document.getElementById(`${row+i}${numeroAAlfabeto(nCol-i)}`).style.backgroundColor = colorOpciones
+        }else if ((row+i) < tamanoTableroLargo+1 && (nCol-i) >= 1 && document.getElementById(`${row+i}${numeroAAlfabeto(nCol-i)}`).innerText !== 0) {
+            document.getElementById(`${row+i}${numeroAAlfabeto(nCol-i)}`).style.backgroundColor = colorOpciones
+            break
+        }
+    }
+    //suroeste
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row-i}${numeroAAlfabeto(nCol-i)}`)){
+            break
+        }
+        if ((row-i) >= 1 && (nCol-i) >= 1 && document.getElementById(`${row-i}${numeroAAlfabeto(nCol-i)}`).innerText == 0) {
+            document.getElementById(`${row-i}${numeroAAlfabeto(nCol-i)}`).style.backgroundColor = colorOpciones
+        }else if ((row-i) >= 1 && (nCol-i) >= 1 && document.getElementById(`${row-i}${numeroAAlfabeto(nCol-i)}`).innerText !== 0) {
+            document.getElementById(`${row-i}${numeroAAlfabeto(nCol-i)}`).style.backgroundColor = colorOpciones
+            break
+        }
+
+    }
+
     //Norte
-    if (row < tamanoTableroLargo) {
-        console.log(`${ row+1}${numeroAAlfabeto(nCol)}`)
-        if(!validaPiezaMontana(`${row+1}${numeroAAlfabeto(nCol)}`)){
-            document.getElementById(`${row+1}${numeroAAlfabeto(nCol)}`).style.backgroundColor = colorOpciones
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row+i}${numeroAAlfabeto(nCol)}`)){
+            break
+        }
+        if ((row+i) < tamanoTableroLargo+1 && document.getElementById(`${row+i}${numeroAAlfabeto(nCol)}`).innerText == 0) {
+            document.getElementById(`${row+i}${numeroAAlfabeto(nCol)}`).style.backgroundColor = colorOpciones
+        }else if ((row+i) < tamanoTableroLargo+1 && (nCol) && document.getElementById(`${row+i}${numeroAAlfabeto(nCol)}`).innerText !== 0) {
+            document.getElementById(`${row+i}${numeroAAlfabeto(nCol)}`).style.backgroundColor = colorOpciones
+            break
         }
     }
-    //Sur
-    if (row > 1) {
-        if(!validaPiezaMontana(`${row-1}${numeroAAlfabeto(nCol)}`)){
-            document.getElementById(`${row-1}${numeroAAlfabeto(nCol)}`).style.backgroundColor = colorOpciones
+    //sur
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row-i}${numeroAAlfabeto(nCol)}`)){
+            break
+        }
+        if ((row-i) >=1 && document.getElementById(`${row-i}${numeroAAlfabeto(nCol)}`).innerText == 0) {
+            document.getElementById(`${row-i}${numeroAAlfabeto(nCol)}`).style.backgroundColor = colorOpciones
+        }else if ((row-i) >= 1 && document.getElementById(`${row-i}${numeroAAlfabeto(nCol)}`).innerText !== 0) {
+            document.getElementById(`${row-i}${numeroAAlfabeto(nCol)}`).style.backgroundColor = colorOpciones
+            break
         }
     }
-    
-    if (row > 1 && nCol < tamanoTableroAncho) {
-        if(!validaPiezaMontana(`${row-1}${numeroAAlfabeto(nCol+1)}`)){
-            document.getElementById(`${row-1}${numeroAAlfabeto(nCol+1)}`).style.backgroundColor = colorOpciones
+
+    //Este
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row}${numeroAAlfabeto(nCol+i)}`)){
+            break
+        }
+        if ((nCol+i) < tamanoTableroAncho+1 && document.getElementById(`${row}${numeroAAlfabeto(nCol+i)}`).innerText == 0) {
+            document.getElementById(`${row}${numeroAAlfabeto(nCol+i)}`).style.backgroundColor = colorOpciones
+        }else if ((nCol+i) < tamanoTableroAncho+1 && document.getElementById(`${row}${numeroAAlfabeto(nCol+i)}`).innerText !== 0) {
+            document.getElementById(`${row}${numeroAAlfabeto(nCol+i)}`).style.backgroundColor = colorOpciones
+            break
         }
     }
-    if (row > 1 && nCol > 1) {
-        if(!validaPiezaMontana(`${row-1}${numeroAAlfabeto(nCol-1)}`)){
-            document.getElementById(`${row-1}${numeroAAlfabeto(nCol-1)}`).style.backgroundColor = colorOpciones
+
+    //Oeste
+    for (let i = 1; i < 3; i++) {
+        if(validaPiezaMontana(`${row}${numeroAAlfabeto(nCol-i)}`)){
+            break
         }
-    }
-    if (row < tamanoTableroLargo && nCol < tamanoTableroAncho) {
-        if(!validaPiezaMontana(`${row+1}${numeroAAlfabeto(nCol+1)}`)){
-            document.getElementById(`${row+1}${numeroAAlfabeto(nCol+1)}`).style.backgroundColor = colorOpciones
-        }
-    }
-    if (row < tamanoTableroLargo && nCol > 1) {
-        if(!validaPiezaMontana(`${row+1}${numeroAAlfabeto(nCol-1)}`)){
-            document.getElementById(`${row+1}${numeroAAlfabeto(nCol-1)}`).style.backgroundColor = colorOpciones
+        if ((nCol-i) >= 1 && document.getElementById(`${row}${numeroAAlfabeto(nCol-i)}`).innerText == 0) {
+            document.getElementById(`${row}${numeroAAlfabeto(nCol-i)}`).style.backgroundColor = colorOpciones
+        }else if ((nCol-i) >= 1 && document.getElementById(`${row}${numeroAAlfabeto(nCol-i)}`).innerText !== 0) {
+            document.getElementById(`${row}${numeroAAlfabeto(nCol-i)}`).style.backgroundColor = colorOpciones
+            break
         }
     }
 
