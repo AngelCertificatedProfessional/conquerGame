018132a6bc84837de6177f3117e08c51740b2ea2
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2023-02-17 00:13:40 -0800

    Se agrego nueva configuracion para entrar al nuevo juego

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -7,9 +7,18 @@ const Home = () => {
     {
       tipo: 1, //juego
       titulo: "Conquer Game",
-      img: "conquerGame.jpg",
+      img: "tituloJuegos/conquerGame.jpg",
       descripcion:
         "Juego de estrategia basado en conquistas y ajedrez, de 2 a 6 jugadores",
+      enviar:"ConquerGameOpciones",
+    },
+    {
+      tipo: 1, //juego
+      titulo: "Mi asquerosa vida laboral",
+      img: "tituloJuegos/Worker1.png",
+      descripcion:
+        "En este juego te tocara trabajar como un empleado de nivel inferior hasta llegar a ser jefe supremo",
+      enviar:"MiAsquerosaVidaLaboral"
     },
   ];
   return (
