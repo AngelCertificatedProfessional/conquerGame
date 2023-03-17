c4f52d1d43d5d4d4e4ab49f951df85ff3d7382b2
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2023-03-16 23:46:17 -0700

    Se modifico el segmento del login para separar el metodo del login del de utilerias pagina

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,6 +1,7 @@
 //Material UI
 import React, { useState, useEffect, lazy, Suspense } from "react";
 import { useNavigate } from "react-router-dom";
+import { numeroMes } from "../utils/UtileriasPagina";
 const LoginFormulario = lazy(() =>
   import("./../components/usuario/LoginFormulario")
 );
@@ -40,32 +41,7 @@ const Login = () => {
     if(mesProximo >= 12){
       mesProximo = 0;
     }
-    switch(mesProximo){
-      case 0:
-        return "Enero"
-      case 1:
-        return "Febrero"
-      case 2:
-        return "Marzo"
-      case 3:
-        return "Abril"
-      case 4:
-        return "Mayo"
-      case 5:
-        return "Junio"
-      case 6:
-        return "Julio"
-      case 7:
-        return "Agosto"
-      case 8:
-        return "Septiembre"
-      case 9:
-        return "Octubre"
-      case 10:
-        return "Noviembre"
-      case 11:
-        return "Diciembre"
-    }
+    return numeroMes(mesProximo)
   }
 
   return (
