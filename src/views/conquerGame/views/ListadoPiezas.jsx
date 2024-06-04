7bb5f62a224d476a58a0dfe50445f6f18777969b
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2024-06-03 23:06:03 -0700

    Se agrega opciones para el listado de conquerGame

---
+++ src/views/conquerGame/views/ListadoPiezas.jsx
@@ -0,0 +1,37 @@
+import { Card, CardActionArea, CardMedia } from "@mui/material";
+import { useEffect, useState } from "react";
+export const ListadoPiezas = ({ nombreImagen, color }) => {
+    const [image, setImage] = useState(null)
+
+    const fetchImage = async () => {
+        try {
+            const response = await import(`../../../images/conquerGame/${color}${nombreImagen}.png`) // change relative path to suit your needs
+            setImage(response.default)
+        } catch (err) {
+        } finally {
+        }
+    }
+
+    useEffect(() => {
+        fetchImage()
+    }, [])
+    return (
+        <Card
+            sx={{ width: 80, flexGrow: { xs: 1, sm: 0 }, height: 80, display: 'flex', flexDirection: 'column' }}
+        >
+            <CardActionArea >
+                <CardMedia
+                    height="80"
+                    width="80"
+                    component="img"
+                    image={image}
+                />
+            </CardActionArea>
+        </Card>
+
+    );
+}
+
+
+
+
