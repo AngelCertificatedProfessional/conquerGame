66f2c559ae504d9d30e113eb4c0b060b6a07fd5b
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2024-07-24 21:29:25 -0700

    Se agrega la funcionalidad para poder ver imagenes en el tablero que representen las figuras

--- src/presentation/conquerGame/views/CuadroMapa.jsx
+++ src/presentation/conquerGame/views/CuadroMapa.jsx
@@ -1,7 +1,12 @@
 import { Box, Tooltip } from "@mui/material";
-import { COLORDISPARO, COLORLAGO, COLORMONTANA, COLORMOVIMIENTOOPCIONES, COLORSELECCIONADOTABLERO, COLORTABLERO, lagosConquerGame, montanasConquerGame } from "../../../types";
+import {
+    COLORDISPARO, COLORMOVIMIENTOOPCIONES,
+    COLORSELECCIONADOTABLERO, lagosConquerGame, montanasConquerGame
+} from "../../../types";
 import { useEffect, useState } from "react";
-
+import Tierra from '../../../images/conquerGame/escenario/tierra.png'
+import Lago from '../../../images/conquerGame/escenario/lago.png'
+import Montana from '../../../images/conquerGame/escenario/montana.png'
 export const CuadroMapa = ({ posicion, handleClick,
     bAreaNoSeleccionable, posicionesPiezaMoverse,
     posicionesPiezaDisparar, posicionPiezaSeleccionada, piezasJugador }) => {
@@ -18,38 +23,50 @@ export const CuadroMapa = ({ posicion, handleClick,
             sx={{
                 width: 40,
                 height: 40,
-                backgroundColor:
-                    posicionesPiezaMoverse.includes(posicion)
-                        ? COLORMOVIMIENTOOPCIONES : posicionPiezaSeleccionada === posicion
-                            ? COLORSELECCIONADOTABLERO : posicionesPiezaDisparar.includes(posicion)
-                                ? COLORDISPARO : montanasConquerGame.includes(posicion)
-                                    ? COLORMONTANA : lagosConquerGame.includes(posicion)
-                                        ? COLORLAGO : COLORTABLERO,
                 border: '1px',
                 borderStyle: 'solid',
-                opacity: bAreaNoSeleccionable ? "50%" : "100%"
+                opacity: bAreaNoSeleccionable ? "50%" : "100%",
+                background:
+                    montanasConquerGame.includes(posicion) ? `url(${Montana})` :
+                        lagosConquerGame.includes(posicion) ?
+                            `url(${Lago})` : `url(${Tierra})`
+
             }}
             onClick={() => bAreaNoSeleccionable ? {} : handleClick(posicion)}
         >
-            {imagen !== '' &&
-                <Tooltip title={titulo} PopperProps={{
-                    modifiers: [
-                        {
-                            name: "offset",
-                            options: {
-                                offset: [0, 25],
+            <Box
+                sx={{
+                    width: 40,
+                    height: 40,
+                    backgroundColor:
+                        posicionesPiezaMoverse.includes(posicion)
+                            ? COLORMOVIMIENTOOPCIONES : posicionPiezaSeleccionada === posicion
+                                ? COLORSELECCIONADOTABLERO : posicionesPiezaDisparar.includes(posicion)
+                                    ? COLORDISPARO : '',
+
+                }}
+            >
+                {imagen !== '' &&
+                    <Tooltip title={titulo} PopperProps={{
+                        modifiers: [
+                            {
+                                name: "offset",
+                                options: {
+                                    offset: [0, 25],
+                                },
                             },
-                        },
-                    ],
-                }}>
-                    <Box
-                        component="img"
-                        height="100%"
-                        width="100%"
-                        src={imagen}
-                    />
-                </Tooltip>
-            }
+                        ],
+                    }}>
+                        <Box
+                            sx={{ opacity: "100%" }}
+                            component="img"
+                            height="100%"
+                            width="100%"
+                            src={imagen}
+                        />
+                    </Tooltip>
+                }
+            </Box>
         </Box>
     );
 }
